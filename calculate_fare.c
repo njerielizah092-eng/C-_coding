7890c329ff354e744aa507021f00b9b23e210aac
njerielizah092-eng <njerielizah092@gmail.com>
2025-10-23 23:03:05 +0300

    Add files via upload

---
+++ calculate_fare.c
@@ -0,0 +1,24 @@
+/*
+Name:Elizabeth Njeri
+Reg no:CT101/G/26440/25
+Description:fare
+*/
+#include <stdio.h>
+
+float calculateFare(float distance);
+int main ()
+{
+float distance;
+
+printf("Enter distance travelled(in km)");
+
+scanf("Total Fare=Ksh.%2f\n");
+
+printf("assume the fare rate is<=50");
+scanf("%f.<=50");
+
+return 0;
+
+}
+
+    
